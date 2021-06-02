485900d40c004dd192aea7c58980b6b6389b4089
Mukul Agrawal <mukul.agrawal06@gmail.com>
2021-06-03 00:53:12 +0530

    7

---
+++ src/TestFiles/NewTest.java
@@ -0,0 +1,15 @@
+package TestFiles;
+
+import org.testng.annotations.DataProvider;
+import org.testng.annotations.Test;
+
+public class NewTest {
+    @Test(dataProvider = "dp")
+    public void f(Integer n, String s) {
+    }
+
+    @DataProvider
+    public Object[][] dp() {
+        return new Object[][] { new Object[] { 1, "a" }, new Object[] { 2, "b" }, };
+    }
+}
