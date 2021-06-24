10f1bc375c99dd5dd8852aef79a97c3858e86ffb
Mukul Agrawal <mukul.agrawal06@gmail.com>
2021-06-24 11:52:40 +0530

    Adding UML also

--- src/TestFiles/MethodCheck.java
+++ src/TestFiles/MethodCheck.java
@@ -3,6 +3,12 @@
  */
 package TestFiles;
 
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.asserts.Assertion;
+
 import DateAndTimeFormat.DateAndTimeFormatExtentReport;
 
 public class MethodCheck {
@@ -10,8 +16,23 @@ public class MethodCheck {
     public static void main(String[] args) {
         // TODO Auto-generated method stub
 
-        DateAndTimeFormatExtentReport dateAndTime = new DateAndTimeFormatExtentReport();
-        System.out.println(dateAndTime.getDateAndTime());
+
+      
+        //DateAndTimeFormatExtentReport dateAndTime = new DateAndTimeFormatExtentReport();
+        //System.out.println(dateAndTime.getDateAndTime());
+    	System.setProperty("webdriver.chrome.driver", "DriverFile//chromedriver.exe");
+        WebDriver driver = new ChromeDriver();
+        driver.get("https://www.google.com/");
+        WebElement MukshiEle = driver.findElement(By.name("q"));
+        MukshiEle.sendKeys("Shivanshi");
+        
+        driver.findElement(By.name("q")).clear();
+        
+        driver.findElement(By.name("q")).sendKeys("Mukul");
+        
+        
+        Assertion assert1 = new Assertion();
+        assert1.assertEquals("123", "123");
     }
 
 }
